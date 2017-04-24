ffb56052e61c1c72fc497ddd99c12277a8f4b024
Leon Zhou <leon.zhou.d@gmail.com>
2017-04-23 23:34:00 -0700

    Initial commit.

---
+++ smartapps/leongenius/garage-controller.src/garage-controller.groovy
@@ -0,0 +1,53 @@
+/**
+ *  Garage Controller
+ *
+ *  Copyright 2017 Leon Zhou
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "Garage Controller",
+    namespace: "leongenius",
+    author: "Leon Zhou",
+    description: "Control my garage doors",
+    category: "Convenience",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+	section("Door config") {
+		// TODO: put inputs here
+        input("theSensor", "capability.contactSensor", required: true, title: "sensor")
+        input("theSwitch", "capability.momentary", required: true, title: "switch")
+
+	}
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+	// TODO: subscribe to attributes, devices, locations, etc.
+}
+
+// TODO: implement event handlers
